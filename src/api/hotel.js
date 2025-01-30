7d235389638bade4cfb2f039c00284a47883f5b8
DESKTOP-9L1QH26\MSI <isurugayashan179@gmail.com>
2025-01-30 10:58:40 +0530

    Initial commit: Add Hotel management project

---
+++ src/api/hotel.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { createHotel, getAllHotels, getHotelById, deleteHotel, updateHotel } from "../application/hotel.js";
+
+const hotelsRouter = express.Router();
+
+hotelsRouter.get("/", getAllHotels);
+hotelsRouter.get("/:id", getHotelById);
+hotelsRouter.post("/", createHotel);
+hotelsRouter.delete("/:id", deleteHotel);
+hotelsRouter.put("/:id", updateHotel);
+
+//Aditional method
+
+// hotelsRouter.route("/").get(getAllHotels).post(createHotel);
+// hotelsRouter
+// .route("/:id")
+// .get(getHotelById)
+// .put(updateHotel)
+// .delete(deleteHotel)
+
+
+export default hotelsRouter; 
---
+++ src/infrastructure/db.js
@@ -0,0 +1,19 @@
+
+import mongoose from "mongoose";
+
+const  connectDB = async () => {
+    try{
+        const mongodb_url = process.env.mongodb_url;
+        if(!mongodb_url){
+            throw new error("MongoDB_URL is not et")
+        }
+            await mongoose.connect(mongodb_url);
+            console.log("Conncetd to the databases");
+    } catch(error){ 
+            console.log("Error connecting to the database");
+            console.log(error);        
+    }
+};   
+ 
+export default connectDB;
+//Mongo Password: znFrQTBWcFkCERam
